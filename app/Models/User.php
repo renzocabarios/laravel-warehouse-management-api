ab1e6cc91b2978f1fba2de438e0c6d25fe156800
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-11-20 18:38:04 +0800

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,13 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    public $primarykey = 'id';
+
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
@@ -30,15 +26,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
