e345cc588ba04fdb33d317e763c803730d68d5f6
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-11-21 11:26:04 +0800

    Add vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: ["resources/routes/**", "routes/**", "resources/views/**"],
             refresh: true,
         }),
     ],
