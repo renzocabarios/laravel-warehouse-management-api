cca962ba159d16b4ec4bd41d0eb96b9a419c876c
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-12-27 17:31:19 +0800

    Add ViewController

---
+++ app/Http/Controllers/ViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ViewController extends Controller
+{
+    public function login()
+    {
+        return view('welcome');
+    }
+}
