02b3d63e173d74fe2d0d468f5d20a7231e770a96
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-12-28 19:59:20 +0800

    Add create item modal in item view

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,23 +1,52 @@
 @extends('master')
+
 @section('content')
 
-<div class="h-full w-full p-4">
-    <table id="myTable" class="drop-shadow-2xl ">
+<div class="h-full w-full p-2">
+
+<button type="button" class="btn btn-primary my-2" data-bs-toggle="modal" data-bs-target="#formModal">
+Add Item
+</button>
+
+<table id="myTable" class="drop-shadow-2xl ">
     <thead>
-    <tr>
-    <th>id</th>
-    <th>Name</th>
-    <th>Description</th>
-    </tr>
-</thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+        </tr>
+    </thead>
 </table>
+
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="formModalLabel">Add Item</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Item Name</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Item Description</label>
+                        <input type="text" class="form-control" id="description" name="description">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 @endsection()
 
 @push('scripts')
-
 <script>
-    $(document).ready(function () {
+    $(document).ready(() => {
         $('#myTable').DataTable({
             ajax: 'api/item',
             columns: [
@@ -27,6 +56,5 @@
             ],
         });
     });
-
 </script>
 @endpush
