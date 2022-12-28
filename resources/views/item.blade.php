6027247dd63f88e6f7ee56d1efbcdd87120415a2
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-12-28 21:45:12 +0800

    Add edit item in item table

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -42,6 +42,34 @@ Add Item
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="formEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="formEditModalLabel">Edit Item</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="editForm">
+                    <div class="mb-3">
+                        <label for="editId" class="form-label">Item Name</label>
+                        <input type="text" class="form-control" id="editId" name="id">
+                    </div>
+                    <div class="mb-3">
+                        <label for="editName" class="form-label">Item Name</label>
+                        <input type="text" class="form-control" id="editName" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="editDescription" class="form-label">Item Description</label>
+                        <input type="text" class="form-control" id="editDescription" name="description">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 @endsection()
 
@@ -64,6 +92,7 @@ Add Item
 
 
         $('.form').on('submit', function (event) {
+            event.preventDefault();
             $.ajax({
                 type: "POST",
                 dataType: 'json',
@@ -78,6 +107,41 @@ Add Item
                 }
             });
         });
+
+        $('.editForm').on('submit', function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: "PATCH",
+                dataType: 'json',
+                url: `/api/item/${$('#editId').val()}`,
+                data: $(this).serialize(),
+                success: function (data) {
+                    window.location.replace(window.location.href);
+                },
+                error: function (data) {
+                    var errors = data.responseJSON;
+                    console.log(errors);
+                }
+            });
+        });
+
+        $(document).on('click', '.edit', function (event) {
+            const id = $(this).attr('id');
+            $.ajax({
+                url: `/api/item/${id}`,
+                dataType: "json",
+                success: function (data) {
+                    $('#editId').val(data.data[0].id);
+                    $('#editName').val(data.data[0].name);
+                    $('#editDescription').val(data.data[0].description);
+                },
+                error: function (data) {
+                    var errors = data.responseJSON;
+                    console.log(errors);
+                }
+            })
+        });
+
 
         $(document).on('click', '.destroy', function (event) {
             event.preventDefault();
