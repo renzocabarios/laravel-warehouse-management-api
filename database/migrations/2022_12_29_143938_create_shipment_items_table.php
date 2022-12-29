d21fbdd0fd4abb50ccb00c39183e4fae0794a43f
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-12-29 17:14:52 +0800

    Update shipmentItems migration

--- database/migrations/2022_12_29_143938_create_shipment_items_table.php
+++ database/migrations/2022_12_29_143938_create_shipment_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,9 +14,15 @@ return new class extends Migration
     {
         Schema::create('shipment_items', function (Blueprint $table) {
             $table->id();
-            $table->integer('itemId');
+
+            $table->unsignedBigInteger('itemId');
+            $table->foreign('itemId')->references('id')->on('items')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('shipmentId');
+            $table->foreign('shipmentId')->references('id')->on('shipments')->cascadeOnDelete();
+
             $table->integer('quantity');
-            $table->integer('shipmentId');
+
             $table->timestamps();
         });
     }
