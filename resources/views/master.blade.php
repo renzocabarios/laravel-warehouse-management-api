3561719ca2a1c608af1b7038387aa674374bb045
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-12-29 16:32:43 +0800

    Update navbar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,8 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </script>
-  <meta name="csrf-token"
-    content="{{ csrf_token() }}" />
+  <meta name="csrf-token" content="{{ csrf_token() }}" />
 @vite('resources/css/app.css')
 </head>
 
@@ -27,6 +26,12 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" href="/item">Items</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/user">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin">Admins</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
