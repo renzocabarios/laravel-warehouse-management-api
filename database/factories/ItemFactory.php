e488156e2b3310a564b97c545863dfabc308b563
Ronny <daveavrosia@gmail.com>
2022-12-29 23:55:30 +0800

    Add item factory

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->sentence(),
+            'image' => $this->faker->image(),
+        ];
+    }
+}
