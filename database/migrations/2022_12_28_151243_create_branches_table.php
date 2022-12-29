b86db62d3b17227b43f614afce959297b59130af
Ronny <daveavrosia@gmail.com>
2022-12-29 17:22:03 +0800

    Add foreign key to branch table

---
+++ database/migrations/2022_12_28_151243_create_branches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('branchOwnerId');
+            $table->foreign('branchOwnerId')->references('id')->on('branch_owners')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('address');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+};
